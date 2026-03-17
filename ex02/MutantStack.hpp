a8fac162cbf84e64146fa9347a50f10e70d39fd2
jimpa <jimmyparcero@protonmail.com>
2026-03-17 17:42:52 +0100

    ex02 done

---
+++ ex02/MutantStack.hpp
@@ -0,0 +1,42 @@
+#ifndef MUTANTSTACK_HPP
+#define MUTANTSTACK_HPP
+
+#include<stack>
+#include<iterator>
+#include <iostream>
+
+template<typename T>
+class MutantStack : public std::stack<T>
+{
+	private:
+
+	public:
+	
+		typedef typename std::stack<T>::container_type::iterator iterator;
+
+		MutantStack() : std::stack<T>()
+		{
+		}
+		MutantStack(const MutantStack &other) : std::stack<T>(other)
+		{
+		}
+		MutantStack &operator=(const MutantStack &other)
+		{
+			if(this != &other)
+				std::stack<T>::operator=(other);
+			return (*this);
+		}
+		iterator begin()
+		{
+			return (this->c.begin());
+		}
+		iterator end()
+		{
+			return (this->c.end());
+		}
+		~MutantStack()
+		{
+		}
+};
+
+#endif
